6ca1953914f4fde069e4374f1911a566b3094dcc
Gavin Stark <gjstark@me.com>
2018-01-31 14:27:06 +0000

    [test] Adding missing files

---
+++ src/ogl_gui/ogl_gui.ml
@@ -0,0 +1,16 @@
+open Stylesheet
+open Ogl_widget
+
+module Stylesheet      = Stylesheet
+module Styleable_value = Styleable_value
+
+module Utils      = Utils
+module Types      = Ogl_types
+module Layout     = Ogl_layout
+module Program    = Ogl_program
+module Widget     = Ogl_widget
+module Obj        = Ogl_obj
+module App        = Ogl_app
+
+let create_stylesheet = Ogl_widget.create_stylesheet
+
